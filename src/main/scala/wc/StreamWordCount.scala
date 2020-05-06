b76fb1c73cb7d5609cabc560153a2207078b5776
wanghy <wanghaoyu349@126.com>
2020-05-06 22:32:29 +0800

    host port

--- src/main/scala/wc/StreamWordCount.scala
+++ src/main/scala/wc/StreamWordCount.scala
@@ -1,14 +1,19 @@
 package wc
 
+import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.streaming.api.scala._
 
 object StreamWordCount {
   def main(args: Array[String]): Unit = {
+    val params = ParameterTool.fromArgs(args)
+    val host: String = params.get("host")
+    val port: Int = params.getInt("port")
+
     // 创建流处理执行环境
     val env = StreamExecutionEnvironment.getExecutionEnvironment
 
     // 接收一个text文本流
-    val dataStream = env.socketTextStream("127.0.0.1", 7788)
+    val dataStream = env.socketTextStream(host, port)
 
     // 对每条数据处理
     val wordCountDataStream = dataStream.flatMap(_.split(" "))
